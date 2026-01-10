793172b1ba94af6453780db5eb662bf247c6ed2c
Pavan Kumar B H <paven026@gmail.com>
2026-01-09 20:46:01 -0600

    Modify CORS middleware to allow origin regex
    Updated CORS middleware configuration to use regex for origins.

--- backend/app/main.py
+++ backend/app/main.py
@@ -34,7 +34,8 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    # allow_origins=origins,
+    allow_origin_regex=".*",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
